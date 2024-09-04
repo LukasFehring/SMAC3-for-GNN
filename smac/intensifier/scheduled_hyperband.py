7715ee9fb155b335e30afc0f31e4bbdbc8619226
Lukas Fehring <lukas.fehring@stud.uni-hannover.de>
2024-09-04 16:23:36 +0200

    Update budget computation

--- smac/intensifier/scheduled_hyperband.py
+++ smac/intensifier/scheduled_hyperband.py
@@ -19,7 +19,7 @@ class ScheduledHyperband(Hyperband):
 
         self._n_configs_in_stage: Dict[int, list] = self.get_n_configs()
         self._budgets_in_stage: Dict[int, list] = self.get_budgets()
-        self._max_iterations: Dict[int, int] = self.get_max_iterations()
+        self._max_iterations: Dict[int, int] = self._get_max_iterations()
 
     def get_budgets(self):
         return {0: list(range(self._min_budget, self._max_budget + 1))}
@@ -27,12 +27,12 @@ class ScheduledHyperband(Hyperband):
     def get_n_configs(self):
         return {0: [math.floor(self._n_lowest_budget / (self._eta**i)) for i in range(self._s_max)]}
 
-    def get_max_iterations(self):
-        return {0: self._max_budget}
+    def _get_max_iterations(self, *args, **kwargs):
+        return self._max_budget - self._min_budget + 1
 
     @property
     def n_configs(self):
         return len(self.get_n_configs()[0])
-    
+
     def _get_next_bracket(self) -> int:
         return 0
